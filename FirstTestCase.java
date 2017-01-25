60e4f17bbdd77c07a90374afc7169e70db601697
qasource-sogupta <sogupta@qasource.com>
2017-01-25 12:07:42 +0530

    1
    1

--- FirstTestCase.java
+++ FirstTestCase.java
@@ -1,29 +1,41 @@
-package automationFramework;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 public class FirstTestCase {
- 
-	public static void main(String[] args) {
-		
-		// Create a new instance of the Firefox driver
-		WebDriver driver = new FirefoxDriver();
-		
-        //Launch the Online Store Website
-		driver.get("http://www.gmail.com");
-		
-		String title = driver.getTitle();
-		
-		if( title == "Gmail")
-			System.out.println("Pass");
-		else {
-			System.out.println("Fail");
-		}
-			
-        // Print a Log In message to the screen
-        System.out.println("Successfully opened the website ");
-        // Close the driver
-        driver.quit();
+    public static void main(String[] args) {
+        int score = 0;
+        int scoreMax = 50;
+
+        // Fibonacci
+        // Test 0
+        if (FibonacciRecursive.fibonacci(0) == 0) {
+            System.out.println("Test 1 Passed +10");
+            score += 10;
+        }
+        // Test 1
+        if (FibonacciRecursive.fibonacci(1) == 1) {
+            System.out.println("Test 2 Passed +10");
+            score += 10;
+        }
+        // Test 5
+        if (FibonacciRecursive.fibonacci(5) == 5) {	
+            System.out.println("Test 3 Passed +10");
+            score += 10;
+        }
+        // Test 10
+        if (FibonacciRecursive.fibonacci(10) == 55) {
+            System.out.println("Test 4 Passed +10");
+            score += 10;
+        }
+        // Test -1
+        if (FibonacciRecursive.fibonacci(-1) == 1) {
+            System.out.println("Test 5 Passed +10");
+            score += 10;
+        }
+
+        if (score != scoreMax) {
+            System.out.println("Score: " + score + "/" + scoreMax);
+            System.exit(1);
+        } else {
+            System.out.println("Score: " + score + "/" + scoreMax);
+            System.exit(0);
+        }
     }
 }
