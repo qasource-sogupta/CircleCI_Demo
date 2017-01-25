495da063a9496fed5f1fe19ca5ebb0864f5e7596
qasource-sogupta <sogupta@qasource.com>
2017-01-25 11:30:59 +0530

    Test

--- src/automationFramework/FirstTestCase.java
+++ src/automationFramework/FirstTestCase.java
@@ -5,20 +5,24 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstTestCase {
  
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		
 		// Create a new instance of the Firefox driver
 		WebDriver driver = new FirefoxDriver();
 		
         //Launch the Online Store Website
 		driver.get("http://www.gmail.com");
- 
-        // Print a Log In message to the screen
-        System.out.println("Successfully opened the website www.Store.Demoqa.com");
- 
-		//Wait for 5 Sec
-		Thread.sleep(5);
 		
+		String title = driver.getTitle();
+		
+		if( title == "Gmail")
+			System.out.println("Pass");
+		else {
+			System.out.println("Fail");
+		}
+			
+        // Print a Log In message to the screen
+        System.out.println("Successfully opened the website ");
         // Close the driver
         driver.quit();
     }
